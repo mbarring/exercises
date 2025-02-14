1e250174dd5de1adfb7e6e30d43bacbbb5c6ed82
mbarring <mark.barrington@optusnet.com.au>
2025-02-14 19:40:33 +1030

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -1,7 +1,8 @@
 git branch branch2
 git checkout branch2
 touch file4
-git commit -am "commit new file4 before modification"
+git add file4
+git commit -m "commit new file4 before modification"
 echo "Hello World" > file4
 git stash
 git checkout master
